28ceed607199f16889d5329b475fa0db5095bd70
winebarrel <sugawara@winebarrel.jp>
2021-11-19 10:21:25 +0900

    Bump up version

--- demitas.rb
+++ demitas.rb
@@ -1,9 +1,9 @@
 class Demitas < Formula
   desc 'Wrapper for ecspresso that creates task definitions at run time.'
   homepage 'https://github.com/winebarrel/demitas'
-  version '0.5.1'
+  version '0.5.2'
   url "https://github.com/winebarrel/demitas/releases/download/v#{version}/demitas_v#{version}_darwin_amd64.tar.gz"
-  sha256 '2d7cfc3e1087efb4168a23e3dbbb991cc6b946071732b6bc63e66c5631c9e721'
+  sha256 'cff266b34360b6535bb5cbc955c449b810dd5c82e6b48e2a7903c4baf3e85e4e'
   license 'MIT'
 
   def install
